7a27aa5608e60ddcd50e33dc34fa4d43dcd55a43
dclandau <diogo.landau@maersk.com>
2022-06-20 02:01:50 +0100

    first validation with fr = cfr

--- dstructures.py
+++ dstructures.py
@@ -22,6 +22,7 @@ class Family:
         tfr: int,
         cfr: int,
         ind_cost: int,
+        revenue: float, 
         fr: Optional[int] = None,
     ): 
         self.family_id = family_id
@@ -32,8 +33,16 @@ class Family:
         self.tfr = int(tfr*100)
         self.cfr = int(cfr*100)
         self.ind_cost = ind_cost
+        self.revenue = revenue
         self.fr = int(cfr*100)
         self.cycles = []
+
+    @property
+    def ST(self): 
+        return functools.reduce(
+            lambda acc, cycle: acc+(cycle.SS + cycle.SC), 
+            self.cycles, 0
+        )
 
     def calculate_cycles(self): 
         start_day = 1
@@ -44,8 +53,9 @@ class Family:
             cycle.calculate_sigma(self.daily_sigma, start_day, self.lt)
             cycle.calculate_BKG(self.fr)
             cycle.calculate_SS()
-            print(self.family_id, cycle.SC, cycle.SS)
+            # print(start_day//(self.lt + self.dp), self.family_id, cycle.SC, cycle.SS)
             start_day += self.lt + self.dp
+
 
 class Cycle: 
 
@@ -54,7 +64,6 @@ class Cycle:
         self.sigma = None
         self.BKG = None
         self.SS = None
-        pass
 
     def calculate_SC(self, daily_demand, start_day, lt): 
         self.SC = sum(daily_demand[start_day-1: start_day-1+lt])
@@ -71,12 +80,30 @@ class Cycle:
     def calculate_SS(self): 
         self.SS = fsolve(
             self.solve_for_ss, 
-            1, 
+            0, 
             args=tuple([self.sigma, self.BKG]),
         )[0]
 
     @staticmethod
     def solve_for_ss(ss, sigma, bkg):
+        global normdist
+        if sigma == 0: 
+            return 0
         return - ss*(1 - normdist.cdf(ss/sigma)) + sigma*normdist.pdf(ss/sigma) - bkg
 
 
+class Plant: 
+
+
+    def __init__(
+        self, plant_id, capacity 
+    ):
+        self.plant_id = plant_id
+        self.capacity = capacity
+
+
+class Budget: 
+
+
+    def __init__(self, budget_per_ton: float): 
+        self.budget_per_ton = budget_per_ton
--- main.py
+++ main.py
@@ -2,83 +2,68 @@ import sys
 import calendar
 import pandas as pd
 import numpy as np 
+import functools
 
 from typing import List
 
-from dstructures import Family
+from dstructures import (
+    Family, Budget, Plant
+)
+from utils import cmp_families, cmp_plants
 
-
-def calculate_ST_in(): 
-    pass
-
-def calculate_SS_in(): 
-    pass
-
-def calculate_BKG_in(): 
-    pass
-
-def calculate_sigma_in(): 
-    pass
-
-def calculate_SC_in(
-    daily_demand_i: List[int], 
-    start_day: int, 
-    lt: int, 
-): 
-    return sum(
-        daily_demand_i[start_day-1:start_day + lt]
-    )
-
-def calculate_family_SC(family: Family):
-    SC_list = []
-    n = 0
-    while True: 
-        start_day=n*(family.lt + family.dp)+1
-        if start_day > 365: 
-            break
-        SC_list.append(
-            calculate_SC_in(
-                daily_demand_i=family.daily_demand,
-                start_day=start_day,
-                lt=family.lt
-            )
-        )
-        n+=1
-
-def get_families_from_excel(file_path: str): 
+def read_input_excel(file_path: str): 
     sheets = pd.read_excel(file_path, sheet_name=None)
     months = pd.DataFrame({
         'Month': pd.Series(range(1, 13))
     })
-    data_fr = sheets["fr"]
+    data_fr = sheets["fr"].loc[:, ["Family", " Target FR", "Critical FR"]]
     data_fr = data_fr.merge(months, how='cross')
-    data_demand = sheets["demand"]
-    data_cycle = sheets["cycle time"]
-    data_cost = sheets["ind cust"]
+    set_family = set(data_fr["Family"].unique())
+    data_demand = sheets["demand"].loc[:, ["Family", "Month", "Demand", "Stdev Demand"]]
+    set_family = set_family & set(data_demand["Family"].unique())
+    data_cycle = sheets["cycle time"].loc[:, ["Family", "Lead Time (days)", "Prod Days"]]
+    set_family = set_family & set(data_cycle["Family"].unique())
+    data_cost = sheets["ind cust"].loc[:, ["Family", "Variable Cost/ Ton", "Fix Cost/ Ton"]]
+    set_family = set_family & set(data_cost["Family"].unique())
+    data_fam_importance = sheets["imp fam"].loc[:, ["Fam", "Revenue"]]
+    data_fam_importance.columns = ["Family", "Revenue"]
+    data_fam_importance.sort_values(["Revenue", "Family"])
+    set_family = set_family & set(data_fam_importance["Family"].unique())
+    data_production = sheets["xij"]
+    set_family = set_family & set(data_production["Family"].unique())
+    data_budget = sheets["bud"]
+    data_capacity = sheets["capfj"]
+
     combined = data_fr.join(
         data_demand.set_index(['Family', 'Month']), on=['Family', 'Month']
     ).reset_index()
     missing_demand = combined['Demand'].isnull()
     combined.loc[missing_demand, 'Demand'] = 0
     combined.loc[missing_demand, 'Stdev Demand'] = 0
-    combined = combined.join(data_cycle.set_index('Family'), on='Family').dropna()
+    combined = combined.join(data_cycle.set_index('Family'), on='Family')
+    combined = combined.dropna()
     combined = combined.join(data_cost.set_index('Family'), on='Family')
-    families = combined.Family.unique()
+    combined = combined.join(data_fam_importance.set_index("Family"), on="Family")
+    combined = combined[combined["Family"].isin(set_family)]
+    set_family = set_family & set(combined["Family"].unique())
+
+    families = set_family
     family_list = []
     for family in families:
         daily_demand_list, daily_sigma_list = [], []
-        family_lt = combined[combined['Family']==family]['Lead Time (days)'].iloc[0]
-        family_dp = combined[combined['Family']==family]['Prod Days'].iloc[0]
-        family_tfr = combined[combined['Family']==family][' Target FR'].iloc[0]
-        family_cfr = combined[combined['Family']==family]['Critical FR'].iloc[0]
-        family_vcost = combined[combined['Family']==family]['Variable Cost/ Ton'].iloc[0]
-        family_fcost = combined[combined['Family']==family]['Fix Cost/ Ton'].iloc[0]
+        family_subset = combined[combined['Family']==family]
+        family_lt = family_subset['Lead Time (days)'].iloc[0]
+        family_dp = family_subset['Prod Days'].iloc[0]
+        family_tfr = family_subset[' Target FR'].iloc[0]
+        family_cfr = family_subset['Critical FR'].iloc[0]
+        family_vcost = family_subset['Variable Cost/ Ton'].iloc[0]
+        family_fcost = family_subset['Fix Cost/ Ton'].iloc[0]
         family_ind_cost = family_vcost + family_fcost
+        family_importance = family_subset["Revenue"].iloc[0]
         for month in range(1, 13): 
             days_in_month = calendar.monthrange(2022, month)[1]
-            [[d, s]] = combined.loc[
-                (combined['Family'] == family)
-                & (combined['Month'] == month), ['Demand', 'Stdev Demand']
+            [[d, s]] = family_subset.loc[
+                (family_subset['Month'] == month), ['Demand', 'Stdev Demand']
             ].to_numpy()
             daily_demand = d/days_in_month
             daily_sigma = s/(days_in_month**(1/2))
@@ -95,15 +80,56 @@ def get_families_from_excel(file_path: str):
             tfr=family_tfr,
             cfr=family_cfr,
             ind_cost=family_ind_cost,
+            revenue=family_importance
         ))
-    return family_list
+
+    plant_list = []
+    for plant in data_capacity.to_dict("records"): 
+        plant_list.append(Plant(
+            plant["Plant"], 
+            plant["Tons/ year"],
+        ))
+
+    data_production = data_production.set_index("Family").loc[
+        data_fam_importance[data_fam_importance["Family"].isin(set_family)]["Family"]
+    ]
+
+    budget = Budget(data_budget.iloc[0, 0])
+
+    return {
+        "family_list": family_list, 
+        "budget": budget,
+        "production_df": data_production,
+        "plant_list": plant_list,
+    }
+
 
 def main(argc, argv): 
-    families = get_families_from_excel(argv[1])
+    res = read_input_excel(argv[1])
+    families = sorted(res["family_list"], key=cmp_families, reverse=True)
+    budget = res["budget"]
+    production_df = res["production_df"]
+    plants = sorted(res["plant_list"], key=cmp_plants) 
 
     for family in families:
         family.calculate_cycles()
-        break
+        
+    for plant in plants: 
+        print(plant.plant_id)
+
+    total_stock = list(map(
+        lambda family: family.ST,
+        families
+    ))
+    plant_restrictions = list(map(
+        lambda plant: plant.capacity, 
+        plants
+    ))
+    stock_vector = np.array([total_stock]).T
+    plant_vector = np.array([plant_restrictions]).T
+    prod_mat = production_df.to_numpy().T
+    print(prod_mat)
+    print(prod_mat.dot(stock_vector) < plant_vector)
 
 if __name__ == '__main__': 
     main(len(sys.argv), sys.argv)
